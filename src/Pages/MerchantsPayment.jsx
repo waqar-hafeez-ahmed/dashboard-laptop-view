53a8acea685a90465e3bbc38fdbe9bbd5b5a751c
Waqar Ahmed <waqarhafeez12@gmail.com>
2023-09-12 10:44:36 +0500

    Made changed to Merchant Payment

--- src/Pages/MerchantsPayment.jsx
+++ src/Pages/MerchantsPayment.jsx
@@ -1,5 +1,11 @@
 const MerchantsPayment = () => {
-  return <div>MerchantsPayment</div>;
+  return (
+    <section className="flex section">
+      <h2 className="text-2xl font-semibold font-Poppins text-primary ml-8 mt-2">
+        Merchants Payment
+      </h2>
+    </section>
+  );
 };
 
 export default MerchantsPayment;
